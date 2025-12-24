39fd35b58b70d2a09e6359d8d5f29aa061153da4
MohamedNusaif <mrmnusaif@gmail.com>
2025-12-24 18:02:20 +0530

    docker mail sendding process

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-   output: "export",
-  images: {
-    unoptimized: true,
-  },
+  // Docker / Server / SSR compatible config
 };
 
 export default nextConfig;
